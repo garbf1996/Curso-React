c52a8c512606cba1c130533d382ac6a1bb072ba3
Garber Batista <garbfbatista@gmail.com>
2022-09-25 18:52:27 -0400

    agregar button

--- git-expert-app/src/GifExpertAPP/GifExpertAPP.jsx
+++ git-expert-app/src/GifExpertAPP/GifExpertAPP.jsx
@@ -1,10 +1,36 @@
+import { useState } from "react"
+
 //Cuerpo de index 
 export const GifExpertAPP = () => {
+
+const [categories,setCategories ] = useState(['One Punch','Naruto']);
+
+
+
+const oneAddcategory = ()=>{
+
+  setCategories([...categories,'Call of duty']);
+}
+
+
   return (
     <>
     <h1>GifExpertAPP</h1>
+
+    <button onClick={oneAddcategory}>Agregar</button>
+
+    <ol>
+       {
+      
+      categories.map(category =>{
+        return <li key={category}>{category}</li>
+      })
+
+
+
+       }
+    </ol>
     
     </>
-    
   )
 }
--- git-expert-app/src/main.jsx
+++ git-expert-app/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { GifExpertAPP } from './GifExpertAPP/GifExpertAPP';
-
+import './css/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
