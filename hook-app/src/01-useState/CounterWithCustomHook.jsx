1c7c1dd77704482dfb7a166d1ff6a939faeda176
Garber Batista <garbfbatista@gmail.com>
2022-10-03 18:47:12 -0400

    //Desestructurar en CounterWithCustomHook.jsx

--- hook-app/src/01-useState/CounterWithCustomHook.jsx
+++ hook-app/src/01-useState/CounterWithCustomHook.jsx
@@ -2,16 +2,16 @@ import { useCounter } from "./hooks/useCounter"
 
 
 export const CounterWithCustomHook = () => {
-
-  const {counter} =  useCounter ();
+//Desestructurar en CounterWithCustomHook.jsx
+  const {counter,incremente,decrement,reset} =  useCounter ();
   return (
     <>
      
      <h1>Counter with hook: {counter}</h1>
       <hr></hr>
-       <button>+1</button>
-       <button>Reset</button>
-       <button>-1</button>
+       <button onClick={incremente}>+1</button>
+       <button onClick={reset}>Reset</button>
+       <button onClick={decrement}>-1</button>
 
     </>
   )
--- hook-app/src/01-useState/hooks/useCounter.jsx
+++ hook-app/src/01-useState/hooks/useCounter.jsx
@@ -3,9 +3,31 @@ import { useState } from "react"
 
 export const useCounter = (initialValue = 10) => {
  
-    const [counter,setcounter] = useState(initialValue)
+    const [counter,setcounter] = useState(initialValue);
+
+    //Funcion de autoincremental
+    const incremente = ()=>{
+      setcounter(counter + 2);
+    }
+
+      //Funcion disminuir
+    const decrement = ()=>{
+
+      if(counter === 0) return;
+      setcounter(counter -1);
+    }
+       //Reniciar de incremente
+    const reset = ()=>{
+     setcounter(initialValue)
+    }
 
   return {
     counter,
+    //Funcion de autoincremental
+    incremente,
+    //Funcion disminuir
+    decrement,
+    //Reniciar de incremente
+    reset
   }
 }
