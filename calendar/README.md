762ffc85cbfeed67c8046ba7413996ce78b58004
garber batista <garbfbatista@gmail.com>
2023-01-01 10:52:39 -0400

    configuracion de env

---
+++ calendar/README.md
@@ -0,0 +1,10 @@
+#Calendar
+
+## Development paso
+
+1. Remonbrar el archivo .env.template por .env
+2. Hacer los cambios respectivos en la variables de entorno
+
+...
+VITE_APT_URL=http://localhost:4000/api
+...
--- calendar/src/router/AppRouter.jsx
+++ calendar/src/router/AppRouter.jsx
@@ -1,27 +1,25 @@
-import React from 'react'
-import { Navigate, Route, Routes } from 'react-router'
-import {LoginPage} from '../auth/page/LoginPage'
-import { CalendarPage } from '../calendar/page/CalendarPage';
-
+import React from "react";
+import { Navigate, Route, Routes } from "react-router";
+import { LoginPage } from "../auth/page/LoginPage";
+import { CalendarPage } from "../calendar/page/CalendarPage";
+import { getEnv } from "../helpers";
 
 export const AppRouter = () => {
+  const authStatus = "authenticated";
 
-    const authStatus = 'authenticated';
-  
-    
+  console.log(getEnv("REACT_APP_API_URL"));
+
   return (
+    <Routes>
+      {/*Validar si estoy autenticado*/}
 
-    <Routes> 
-         {/*Validar si estoy autenticado*/}
-       
-       {
-        (authStatus === 'not-authenticated')
-        ?<Route path='/auth/*' element ={<LoginPage/>} />
-        :<Route path='/*' element ={<CalendarPage/>} />
-        } 
-       
+      {authStatus === "not-authenticated" ? (
+        <Route path='/auth/*' element={<LoginPage />} />
+      ) : (
+        <Route path='/*' element={<CalendarPage />} />
+      )}
 
-       <Route path='/*' element ={<Navigate to="/auth/login" />} /> 
+      <Route path='/*' element={<Navigate to='/auth/login' />} />
     </Routes>
-  )
-}
+  );
+};
