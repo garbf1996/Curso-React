96952ad35bada329f14d5b22cd1d9ae8b49acaac
garber batista <garbfbatista@gmail.com>
2023-01-02 10:33:38 -0400

    dispatch

--- calendar/src/store/auth/authSlice.js
+++ calendar/src/store/auth/authSlice.js
@@ -11,15 +11,23 @@ export const authSlice = createSlice({
     onChecking: (state) => {
       state.status = "checking";
       state.user = {};
-      state.errorMessages = undefined;
+      state.errorMessage = undefined;
     },
     onLogin: (state, { payload }) => {
       state.status = "authenticated";
       state.user = payload;
-      state.errorMessages = undefined;
+      state.errorMessage = undefined;
+    },
+    onLogout: (state, { payload }) => {
+      state.status = "not-authenticated";
+      state.user = {};
+      state.errorMessage = payload;
+    },
+    clearError: (state) => {
+      state.errorMessage = undefined;
     },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { onChecking, onLogin } = authSlice.actions;
+export const { onChecking, onLogin, onLogout, clearError } = authSlice.actions;
