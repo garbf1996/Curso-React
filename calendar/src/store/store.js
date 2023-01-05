fe97ab7f9b8111b03667bbd2ed99fbcce1f8bf65
garber batista <garbfbatista@gmail.com>
2023-01-04 22:56:27 -0400

    stores

--- calendar/src/store/store.js
+++ calendar/src/store/store.js
@@ -1,13 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { uiSlice, calendarSlice } from './';
-
+import { configureStore } from "@reduxjs/toolkit";
+import { uiSlice, calendarSlice } from "./";
+import { authSlice } from "./auth/authSlice";
 
 export const store = configureStore({
-    reducer: {
-        calendar: calendarSlice.reducer,
-        ui: uiSlice.reducer
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-        serializableCheck: false
-    })
-})
+  reducer: {
+    auth: authSlice.reducer,
+    calendar: calendarSlice.reducer,
+    ui: uiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
