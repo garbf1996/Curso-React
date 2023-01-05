e91cad8458ac0f1a9c0cef989bec675303811f3a
garber batista <garbfbatista@gmail.com>
2023-01-04 23:50:15 -0400

    Validar

--- calendar/src/auth/pages/LoginPage.jsx
+++ calendar/src/auth/pages/LoginPage.jsx
@@ -1,78 +1,131 @@
-import './LoginPage.css';
+import "./LoginPage.css";
+import { useForm } from "../../hooks";
+
+const loginFormFields = {
+  loginEmail: "",
+  loginPassword: "",
+};
+
+const registerFormFields = {
+  registerName: "",
+  registerEmail: "",
+  registerPassword: "",
+  registerPassword2: "",
+};
 
 export const LoginPage = () => {
-    return (
-        <div className="container login-container">
-            <div className="row">
-                <div className="col-md-6 login-form-1">
-                    <h3>Ingreso</h3>
-                    <form>
-                        <div className="form-group mb-2">
-                            <input 
-                                type="text"
-                                className="form-control"
-                                placeholder="Correo"
-                            />
-                        </div>
-                        <div className="form-group mb-2">
-                            <input
-                                type="password"
-                                className="form-control"
-                                placeholder="Contraseña"
-                            />
-                        </div>
-                        <div className="d-grid gap-2">
-                            <input 
-                                type="submit"
-                                className="btnSubmit"
-                                value="Login" 
-                            />
-                        </div>
-                    </form>
-                </div>
+  const {
+    loginEmail,
+    loginPassword,
+    onInputChange: onloginInputChange,
+  } = useForm(loginFormFields);
 
-                <div className="col-md-6 login-form-2">
-                    <h3>Registro</h3>
-                    <form>
-                        <div className="form-group mb-2">
-                            <input
-                                type="text"
-                                className="form-control"
-                                placeholder="Nombre"
-                            />
-                        </div>
-                        <div className="form-group mb-2">
-                            <input
-                                type="email"
-                                className="form-control"
-                                placeholder="Correo"
-                            />
-                        </div>
-                        <div className="form-group mb-2">
-                            <input
-                                type="password"
-                                className="form-control"
-                                placeholder="Contraseña" 
-                            />
-                        </div>
+  const {
+    registerName,
+    registerEmail,
+    registerPassword,
+    registerPassword2,
+    onInputChange,
+  } = useForm(registerFormFields);
 
-                        <div className="form-group mb-2">
-                            <input
-                                type="password"
-                                className="form-control"
-                                placeholder="Repita la contraseña" 
-                            />
-                        </div>
+  const onSubmitLogin = (e) => {
+    e.preventDefault();
+    //TODO: Validar los campos
 
-                        <div className="d-grid gap-2">
-                            <input 
-                                type="submit" 
-                                className="btnSubmit" 
-                                value="Crear cuenta" />
-                        </div>
-                    </form>
-                </div>
+    if (loginEmail == "" || loginPassword == "") {
+      return alert("Los compos son obligatorios");
+    }
+  };
+
+  const onsubmitRegistre = (e) => {
+    e.preventDefault();
+
+    console.log("hay algo");
+  };
+
+  return (
+    <div className='container login-container'>
+      <div className='row'>
+        <div className='col-md-6 login-form-1'>
+          <h3>Ingreso</h3>
+          <form onSubmit={onSubmitLogin}>
+            <div className='form-group mb-2'>
+              <input
+                type='text'
+                className='form-control'
+                placeholder='Correo'
+                name='loginEmail'
+                value={loginEmail}
+                onChange={onloginInputChange}
+              />
             </div>
+            <div className='form-group mb-2'>
+              <input
+                type='password'
+                className='form-control'
+                placeholder='Contraseña'
+                name='loginPassword'
+                value={loginPassword}
+                onChange={onloginInputChange}
+              />
+            </div>
+            <div className='d-grid gap-2'>
+              <input type='submit' className='btnSubmit' value='Login' />
+            </div>
+          </form>
         </div>
-    )
-}
+
+        <div className='col-md-6 login-form-2'>
+          <h3>Registro</h3>
+          <form onSubmit={onsubmitRegistre}>
+            <div className='form-group mb-2'>
+              <input
+                type='text'
+                className='form-control'
+                placeholder='Nombre'
+                name='registerName'
+                value={registerName}
+                onChange={onInputChange}
+              />
+            </div>
+            <div className='form-group mb-2'>
+              <input
+                type='email'
+                className='form-control'
+                placeholder='Correo'
+                name='registerEmail'
+                value={registerEmail}
+                onChange={onInputChange}
+              />
+            </div>
+            <div className='form-group mb-2'>
+              <input
+                type='password'
+                className='form-control'
+                placeholder='Contraseña'
+                name='registerPassword'
+                value={registerPassword}
+                onChange={onInputChange}
+              />
+            </div>
+
+            <div className='form-group mb-2'>
+              <input
+                type='password'
+                className='form-control'
+                placeholder='Repita la contraseña'
+                name='registerPassword2'
+                value={registerPassword2}
+                onChange={onInputChange}
+              />
+            </div>
+
+            <div className='d-grid gap-2'>
+              <input type='submit' className='btnSubmit' value='Crear cuenta' />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
