811b8d06c931cf39f8176aaed5fa419d01cad05d
garber batista <garbfbatista@gmail.com>
2023-01-08 12:57:48 -0400

    add events the data base

--- calendar/src/hooks/useCalendarStore.js
+++ calendar/src/hooks/useCalendarStore.js
@@ -1,46 +1,54 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent } from '../store';
+import { useDispatch, useSelector } from "react-redux";
+import {
+  onAddNewEvent,
+  onDeleteEvent,
+  onSetActiveEvent,
+  onUpdateEvent,
+} from "../store";
 
+import { calendarApi } from "../api";
 
 export const useCalendarStore = () => {
-  
-    const dispatch = useDispatch();
-    const { events, activeEvent } = useSelector( state => state.calendar );
+  const dispatch = useDispatch();
+  const { events, activeEvent } = useSelector((state) => state.calendar);
 
-    const setActiveEvent = ( calendarEvent ) => {
-        dispatch( onSetActiveEvent( calendarEvent ) )
+  const { user } = useSelector((state) => state.auth);
+
+  const setActiveEvent = (calendarEvent) => {
+    dispatch(onSetActiveEvent(calendarEvent));
+  };
+
+  const startSavingEvent = async (calendarEvent) => {
+    // TODO: llegar al backend
+
+    // Todo bien
+    if (calendarEvent._id) {
+      // Actualizando
+      dispatch(onUpdateEvent({ ...calendarEvent }));
+    } else {
+      // Creando
+      const { data } = await calendarApi.post("/events", calendarEvent);
+
+      console.log({ data });
+      dispatch(onAddNewEvent({ ...calendarEvent, id: data.event.id, user }));
     }
+  };
 
-    const startSavingEvent = async( calendarEvent ) => {
-        // TODO: llegar al backend
+  const startDeletingEvent = () => {
+    // Todo: Llegar al backend
 
-        // Todo bien
-        if( calendarEvent._id ) {
-            // Actualizando
-            dispatch( onUpdateEvent({ ...calendarEvent }) );
-        } else {
-            // Creando
-            dispatch( onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() }) );
-        }
-    }
+    dispatch(onDeleteEvent());
+  };
 
-    const startDeletingEvent = () => {
-        // Todo: Llegar al backend
+  return {
+    //* Propiedades
+    activeEvent,
+    events,
+    hasEventSelected: !!activeEvent,
 
-
-        dispatch( onDeleteEvent() );
-    }
-
-
-    return {
-        //* Propiedades
-        activeEvent,
-        events,
-        hasEventSelected: !!activeEvent,
-
-        //* Métodos
-        startDeletingEvent,
-        setActiveEvent,
-        startSavingEvent,
-    }
-}
+    //* Métodos
+    startDeletingEvent,
+    setActiveEvent,
+    startSavingEvent,
+  };
+};
