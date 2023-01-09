8b51ad864949bf5932b669229a2fb3ea2486747f
garber batista <garbfbatista@gmail.com>
2023-01-08 20:58:40 -0400

    end in canledar

--- calendar/src/hooks/useAuthStore.js
+++ calendar/src/hooks/useAuthStore.js
@@ -1,6 +1,12 @@
 import { useDispatch, useSelector } from "react-redux";
 import { calendarApi } from "../api";
-import { onChecking, onLogin, onLogout, clearErrorMessage } from "../store";
+import {
+  onChecking,
+  onLogin,
+  onLogout,
+  clearErrorMessage,
+  onLogoutCalendar,
+} from "../store";
 
 export const useAuthStore = () => {
   const { status, user, errorMessage } = useSelector((state) => state.auth);
@@ -64,6 +70,7 @@ export const useAuthStore = () => {
   const startLogout = () => {
     localStorage.clear();
     dispatch(onLogout());
+    dispatch(onLogoutCalendar());
   };
 
   return {
--- calendar/src/store/calendar/calendarSlice.js
+++ calendar/src/store/calendar/calendarSlice.js
@@ -44,6 +44,12 @@ export const calendarSlice = createSlice({
         }
       });
     },
+
+    onLogoutCalendar: (state) => {
+      (state.isLoadingEvents = true),
+        (state.events = []),
+        (state.activeEvent = null);
+    },
   },
 });
 
@@ -54,4 +60,5 @@ export const {
   onUpdateEvent,
   onDeleteEvent,
   onLoadEvents,
+  onLogoutCalendar,
 } = calendarSlice.actions;
